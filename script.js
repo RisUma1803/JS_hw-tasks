13d460ff91d8a9e8b8a535e2ec03c4e6e34717ab
-g <-g>
2024-05-26 19:46:31 +0500

    First JS

---
+++ script.js
@@ -0,0 +1,50 @@
+var name = prompt('Ismingiz: ')
+console.log('Ismim ' + name);
+
+var age = prompt('Yoshingiz: ')
+console.log('Yoshim ' + age);
+
+var task1 = prompt('Solve the problem: 23 + 8 = ?')
+console.log('Task 1: 23 + 8 = 31. ' + 'Your answer is ' + task1);
+
+var task2 = prompt('Solve the problem: 56 + 814 = ?')
+console.log('Task 2: 56 + 814 = 870. ' + 'Your answer is ' + task2);
+
+var task3 = prompt('Solve the problem: 23 - 21 = ?')
+console.log('Task 3: 23 - 21 = 2. ' + 'Your answer is ' + task3);
+
+var task4 = prompt('Solve the problem: 561 - 21 = ?')
+console.log('Task 4: 561 - 21 = 540. ' + 'Your answer is ' + task4);
+
+var task5 = prompt('Solve the problem: 1256 - 45 = ?')
+console.log('Task 5: 1256 - 45 = 1211. ' + 'Your answer is ' + task5);
+
+var task6 = prompt('Solve the problem: 2 * 8 = ?')
+console.log('Task 6: 12 * 8 = 16. ' + 'Your answer is ' + task6);
+
+var task7 = prompt('Solve the problem: 23 * 10 = ?')
+console.log('Task 7: 23 * 10 = 230. ' + 'Your answer is ' + task7);
+
+var task8 = prompt('Solve the problem: 63 * 4 = ?')
+console.log('Task 8: 63 * 4 = 252. ' + 'Your answer is ' + task8);
+
+var task9 = prompt('Solve the problem: 25 * 25 = ?')
+console.log('Task 9: 25 * 25 = 625. ' + 'Your answer is ' + task9);
+
+var task10 = prompt('Solve the problem: 221 / 11 = ?')
+console.log('Task 10: 221 / 11 = 11. ' + 'Your answer is ' + task10);
+
+var task11 = prompt('Solve the problem: 84 / 7 = ?')
+console.log('Task 11: 84 / 7 = 12. ' + 'Your answer is ' + task11);
+
+var task12 = prompt('Solve the problem: 1625 / 25 = ?')
+console.log('Task 12: 1625 / 25 = 65. ' + 'Your answer is ' + task12);
+
+var task13 = prompt('Solve the problem: 23 % 8 = ?')
+console.log('Task 1: 23 % 8 = 7. ' + 'Your answer is ' + task13);
+
+var task14 = prompt('Solve the problem: 52 % 11 = ?')
+console.log('Task 14: 52 % 11 = 8. ' + 'Your answer is ' + task14);
+
+var task15 = prompt('Solve the problem: 1234598 % 5 = ?')
+console.log('Task 15: 1234598 % 5 = 3. ' + 'Your answer is ' + task15);
