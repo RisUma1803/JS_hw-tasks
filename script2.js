9de8bff13a0bb7abaca4c5a6dcf34d47b299faba
-g <-g>
2024-06-08 23:32:40 +0500

    combining all js hw tasks into one file

---
+++ script2.js
@@ -0,0 +1,118 @@
+
+// If else
+
+
+// var name = prompt('Tell me Your name: ')
+// if (name == NaN) {
+//     console.log('My name is ' + name);
+// } else {
+//     console.log('Answer should be string'); 
+// }
+
+
+// var age = prompt('Your age: ')
+// console.log('My age is ' + age);
+
+// var task1 = prompt('Solve the problem: 23 + 8 = ?')
+// if (task1 == 31) {
+//     console.log('Task 1: 23 + 8 = 31. ' + ' Your answer is correct ' + task1);
+// } else {
+//     console.log('Your answer is incorrect ' + task1 + ' Correct one is: 31. ');
+// }
+
+// var task2 = prompt('Solve the problem: 56 + 814 = ?')
+// if (task2 == 870) {
+//     console.log('Task 2: 56 + 814 = 870. ' + ' Your answer is  ' + task2);
+// } else {
+//     console.log('Your answer is incorrect ' + task2 + ' Correct one is: 870. ');
+// }
+
+// var task3 = prompt('Solve the problem: 23 - 21 = ?')
+// if (task3 == 2) {
+//     console.log('Task 3: 23 - 21 = 2. ' + ' Your answer is correct ' + task3);
+// } else {
+//     console.log('Your answer is incorrect ' + task3 + ' Correct one is: 2. ');
+// }
+
+// var task4 = prompt('Solve the problem: 561 - 21 = ?')
+// if (task4 == 540) {
+//     console.log('Task 5: 560 - 21 = 540. ' + ' Your answer is correct ' + task4);
+// } else {
+//     console.log('Your answer is incorrect ' + task4 + ' Correct one is: 540. ');
+// }
+
+// var task5 = prompt('Solve the problem: 2 * 8 = ?')
+// if (task5 == 16) {
+//     console.log('Task 5: 2 * 8 = 16. ' + ' Your answer is correct ' + task5);
+// } else {
+//     console.log('Your answer is incorrect ' + task5 + ' Correct one is: 16. ');
+// }
+
+// var task6 = prompt('Solve the problem: 63 * 4 = ?')
+// if (task6 == 252) {
+//     console.log('Task 6: 63 * 4 = 252. ' + ' Your answer is correct ' + task6);
+// } else {
+//     console.log('Your answer is incorrect ' + task6 + ' Correct one is: 252. ');
+// }
+
+// var task7 = prompt('Solve the problem: 221 / 11 = ?')
+// if (task7 == 11) {
+//     console.log('Task7: 221 / 11 = 11. ' + ' Your answer is correct ' + task7);
+// } else {
+//     console.log('Your answer is incorrect ' + task7 + ' Correct one is: 11. ');
+// }
+
+// var task8 = prompt('Solve the problem: 23 % 8 = ?')
+// if (task8 == 7) {
+//     console.log('Task 8: 23 % 8 = 7. ' + ' Your answer is correct ' + task8);
+// } else {
+//     console.log('Your answer is incorrect ' + task8 + ' Correct one is: 7. ');
+// }
+
+// var task9 = prompt('Solve the problem: 1234598 % 5 = ?')
+// if (task9 == 3){
+//     console.log('Task 9: 1234598 % 5 = 3. ' + ' Your answer is correct ' + task9);
+// } else{
+//     console.log('Your answer is incorrect ' + task9 + ' Correct one is: 3. ');
+// }
+
+
+
+
+
+
+// // Part 2. Task 1
+// var age = +prompt('Tell me your age as a number: ')
+
+// if (age > 0 && age <= 18) {
+//     alert("You're young, you have to study yet");
+// } else if (age > 18 && age <= 50) {
+//     alert("You have to work");
+// } else if (age > 50 && age <= 59) {
+//     alert("You'll retire soon");
+// } else if (age > 59 && age <= 150) {
+//     alert("You look like retired, If you alive");
+// } else {
+//     alert("Something went wrong");
+// }
+
+
+
+
+// // Task 2
+
+// var a = +prompt('First number')
+// var b = +prompt('Second number')
+// var c = +prompt('Third number')
+
+// if ((a >= b && a <= c) || (a >= c && a <= b)) {
+//     alert(a)
+// } else if ((b >= a && b <= c) || (b >= c && b <= a)) {
+//     alert(b)
+// } else if ((c >= a && c <= b) || (c >= b && c <= a)) {
+//     alert(c)
+// } else {
+//     alert ('Something wrong')
+// }
+
+
